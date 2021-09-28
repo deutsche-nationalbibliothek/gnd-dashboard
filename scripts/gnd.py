9299149c39159481e8697a5e5fef52337fda9cf0
Nico Wagner <nwagner84@protonmail.com>
2021-09-28 07:41:26 +0200

    Setup poetry project and black formatter

--- scripts/gnd.py
+++ scripts/gnd.py
@@ -10,30 +10,23 @@ def main():
     gnd_data = pd.read_csv("data/user/gnd.csv", low_memory=False)
 
     df_0XXR = pd.read_csv(
-        "data/user/0XXR.csv", names=["idn", "gnd_id", "name", "code"], low_memory=False
+        "data/user/0XXR.csv",
+        names=["idn", "gnd_id", "name", "code"],
+        low_memory=False,
     )
 
     df_0XXR = pd.merge(df_0XXR, gnd_data[["gnd_id", "bbg"]], on="gnd_id", how="left")
 
     # AUSWERTUNG RELATIONS-CODES
 
-    df_codes = (
-        df_0XXR[["idn", "code"]]
-        .groupby(by="code")
-        .count()
-        .sort_values("idn", ascending=False)
-    )
+    df_codes = df_0XXR[["idn", "code"]].groupby(by="code").count().sort_values("idn", ascending=False)
     df_codes = df_codes.rename(columns={"idn": "count"})
     df_codes.to_csv("stats/gnd_codes_all.csv")
     df_codes[:10].to_csv("stats/gnd_codes_top10.csv")
 
     # AUSWERTUNG GND-SYSTEMATIK
-    df = pd.read_csv(
-        "stats/gnd_systematik.csv", low_memory=False, names=["id", "count"]
-    )
-    names = pd.read_csv(
-        "data/gnd_systematik_names.csv", low_memory=False, names=["id", "name"]
-    )
+    df = pd.read_csv("stats/gnd_systematik.csv", low_memory=False, names=["id", "count"])
+    names = pd.read_csv("data/gnd_systematik_names.csv", low_memory=False, names=["id", "name"])
 
     result = pd.merge(df, names, on="id", how="left")
     result.to_csv("stats/gnd_classification_all.csv", index=False)
@@ -42,9 +35,7 @@ def main():
     result.to_csv("stats/gnd_classification_top10.csv", index=False)
 
     # AUSWERTUNG GND-SYSTEMATIK (nur Ts*)
-    df = pd.read_csv(
-        "stats/gnd_systematik_Ts.csv", low_memory=False, names=["id", "count"]
-    )
+    df = pd.read_csv("stats/gnd_systematik_Ts.csv", low_memory=False, names=["id", "count"])
 
     result = pd.merge(df, names, on="id", how="left")
     result.to_csv("stats/gnd_classification_Ts_all.csv", index=False)
